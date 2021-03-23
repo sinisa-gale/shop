51e815ace6346b63ba042b3fc59d4dbd2d683d60
unknown <gacic.sinisa@gmail.com>
2021-03-23 19:48:02 +0100

    class drink

--- ProductManagement/src/labs/pm/data/Drink.java
+++ ProductManagement/src/labs/pm/data/Drink.java
@@ -16,10 +16,16 @@
  */
 package labs.pm.data;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author Sinisa
  */
 public class Drink extends Product {
+
+    public Drink(int id, String name, BigDecimal price, Rating rating) {
+        super(id, name, price, rating);
+    }
     
 }
