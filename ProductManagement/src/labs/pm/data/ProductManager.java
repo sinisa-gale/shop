d3c5ae9e50766c920ac57f903d61971470ccd82d
unknown <gacic.sinisa@gmail.com>
2021-03-26 00:05:05 +0100

    interface 2

--- ProductManagement/src/labs/pm/data/ProductManager.java
+++ ProductManagement/src/labs/pm/data/ProductManager.java
@@ -17,19 +17,44 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  *
  * @author Sinisa
  */
 public class ProductManager {
+    private Product product;
+    private Review review;
+    private Locale locale;
+    private ResourceBundle resources;
+    private DateTimeFormatter dateFormat;
+    private NumberFormat moneyFormat;
+
+    public ProductManager(Locale locale) {
+        this.locale = locale;
+    }
+    
+    
+    
     public Product createProduct(int id, String name, BigDecimal price, Rating rating,
             LocalDate bestBefore) {
-        return new Food(id, name, price, rating, bestBefore);
+        product = new Food(id, name, price, rating, bestBefore);
+        return product;
     }
     
     public Product createProduct(int id, String name, BigDecimal price, Rating rating) {
-        return new Drink(id, name, price, rating);
+        product = new Drink(id, name, price, rating);
+        return product;
+    }
+    
+    public Product reviewProduct(Product product, Rating rating, String comments) {
+        review = new Review(rating, comments);
+        this.product = product.applyRating(rating);
+        return this.product;
     }
 }
