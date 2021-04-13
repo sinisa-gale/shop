1842c9c2ce2c7ccbcef40d420189a12f2a3212f0
sinisa-gale <gacic.sinisa@gmail.com>
2021-04-13 20:51:02 +0200

    Error Handling finished

--- ProductManagement/src/labs/pm/data/ProductManager.java
+++ ProductManagement/src/labs/pm/data/ProductManager.java
@@ -19,8 +19,10 @@ package labs.pm.data;
 import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -40,8 +42,13 @@ import java.util.stream.Collectors;
  * @author Sinisa
  */
 public class ProductManager {
-    
+
     private Map<Product, List<Review>> products = new HashMap<>();
+    private ResourceBundle config = ResourceBundle.getBundle("labs.pm.data.config");
+    private MessageFormat reviewFormat = new MessageFormat(config
+            .getString("review.data.format"));
+    private MessageFormat productFormat = new MessageFormat(config
+            .getString("product.data.format"));
     private ResourceFormatter formatter;
     private static final Logger logger = Logger.getLogger(ProductManager.class.getName());
     private static Map<String, ResourceFormatter> formatters
@@ -50,24 +57,24 @@ public class ProductManager {
                     "fr-FR", new ResourceFormatter(Locale.FRANCE),
                     "ru-RU", new ResourceFormatter(new Locale("ru", "RU")),
                     "zh-CN", new ResourceFormatter(Locale.CHINA));
-    
+
     public ProductManager(Locale locale) {
         this(locale.toLanguageTag());
     }
-    
+
     public ProductManager(String languageTag) {
         changeLocale(languageTag);
     }
-    
+
     public void changeLocale(String languageTag) {
         formatter = formatters.getOrDefault(languageTag,
                 formatters.get("en-GB"));
     }
-    
+
     public static Set<String> getSupportedLocales() {
         return formatters.keySet();
     }
-    
+
     public void printProductReport(int id) {
         try {
             printProductReport(findProduct(id));
@@ -75,7 +82,7 @@ public class ProductManager {
             logger.log(Level.INFO, ex.getMessage());
         }
     }
-    
+
     public void printProductReport(Product product) {
         List<Review> reviews = products.get(product);
         StringBuilder txt = new StringBuilder();
@@ -92,7 +99,7 @@ public class ProductManager {
         }
         System.out.println(txt);
     }
-    
+
     public void printProducts(Comparator<Product> sorter) {
         List<Product> productList = new ArrayList<>(products.keySet());
         productList.sort(sorter);
@@ -103,20 +110,51 @@ public class ProductManager {
         }
         System.out.println(txt);
     }
-    
+
+    public void parseReview(String text) {
+        try {
+            Object[] values = reviewFormat.parse(text);
+            reviewProduct(Integer.parseInt((String) values[0]),
+                    Rateable.convert(Integer.parseInt((String) values[1])),
+                    (String) values[2]);
+        } catch (ParseException | NumberFormatException ex) {
+            logger.log(Level.WARNING, "Error parsing text " + text, ex.getMessage());
+        }
+    }
+
+    public void parseProduct(String text) {
+        try {
+            Object[] values = productFormat.parse(text);
+            int id = Integer.parseInt((String) values[1]);
+            String name = (String) values[2];
+            BigDecimal price = BigDecimal.valueOf(Double.parseDouble((String) values[3]));
+            Rating rating = Rateable.convert(Integer.parseInt((String) values[4]));
+            switch ((String) values[0]) {
+                case "D":
+                    createProduct(id, name, price, rating);
+                    break;
+                case "F":
+                    LocalDate bestBefore = LocalDate.parse((String)values[5]);
+                    createProduct(id, name, price, rating, bestBefore);
+            }
+        } catch (ParseException | NumberFormatException | DateTimeParseException ex) {
+            logger.log(Level.WARNING, "Error parsing text " + text, ex.getMessage());
+        }
+    }
+
     public Product createProduct(int id, String name, BigDecimal price, Rating rating,
             LocalDate bestBefore) {
         Product product = new Food(id, name, price, rating, bestBefore);
         products.putIfAbsent(product, new ArrayList<>());
         return product;
     }
-    
+
     public Product createProduct(int id, String name, BigDecimal price, Rating rating) {
         Product product = new Drink(id, name, price, rating);
         products.putIfAbsent(product, new ArrayList<>());
         return product;
     }
-    
+
     public Product reviewProduct(int id, Rating rating, String comments) {
         try {
             return reviewProduct(findProduct(id), rating, comments);
@@ -125,7 +163,7 @@ public class ProductManager {
         }
         return null;
     }
-    
+
     public Product reviewProduct(Product product, Rating rating, String comments) {
         List<Review> reviews = products.get(product);
         products.remove(product, reviews);
@@ -161,9 +199,9 @@ public class ProductManager {
                 .filter(p -> p.getId() == id)
                 .findFirst()
                 .orElseThrow(
-                        ()->new ProductManagerException("Product with id"+id+" not found."));
+                        () -> new ProductManagerException("Product with id" + id + " not found."));
     }
-    
+
     public Map<String, String> getDiscounts() {
         return products.keySet()
                 .stream()
@@ -175,34 +213,34 @@ public class ProductManager {
                                 discount -> formatter.moneyFormat.format(discount))
                 ));
     }
-    
+
     private static class ResourceFormatter {
-        
+
         private Locale locale;
         private ResourceBundle resources;
         private DateTimeFormatter dateFormat;
         private NumberFormat moneyFormat;
-        
+
         private ResourceFormatter(Locale locale) {
             this.locale = locale;
             resources = ResourceBundle.getBundle("labs.pm.data.resources", locale);
             dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
             moneyFormat = NumberFormat.getCurrencyInstance(locale);
         }
-        
+
         private String formatProduct(Product product) {
             return MessageFormat.format(resources.getString("product"),
                     product.getName(), moneyFormat.format(product.getPrice()),
                     product.getRating().getStars(),
                     dateFormat.format(product.getBestBefore()));
         }
-        
+
         private String formatReview(Review review) {
             return MessageFormat.format(resources.getString("review"),
                     review.getRating().getStars(),
                     review.getComments());
         }
-        
+
         private String getText(String key) {
             return resources.getString(key);
         }
